cfc15e927370e708d0223ec9643892c96db8a34f
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-05-19 13:57:09 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(myArray, stringName){
+
+  return myArray.filter( item => item.toLowerCase() === stringName.toLowerCase()
+  );
+}
+
+function fuzzyMatch(myArray, stringName){
+
+  return myArray.filter( item => item.indexOf(stringName) === 0 )
+
+}
+
+function matchName(driverObj, stringName){
+  return driverObj.filter(item => item.name === stringName)
+}
